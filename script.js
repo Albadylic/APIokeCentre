67e6cfa8a8237a51881d5dcd8b89e54b26dfb9b5
Gregor <gregorlthomson@sky.com>
2019-09-12 22:29:14 +0100

    Add moves section and fetch request

--- script.js
+++ script.js
@@ -24,16 +24,30 @@ const searchByNum = () => {
 
 const populateDOM = obj => {
   // Reset contentSpace
-  const contentSpace = document.getElementById("content");
+  const contentSpace = document.getElementById("pokeContent");
   contentSpace.textContent = "";
 
   // Add Pokemon name and sprite
   const title = document.createElement("h3");
   const image = document.createElement("img");
   title.textContent = obj.name;
+  title.classList.add("pokemonName");
   contentSpace.appendChild(title);
   image.src = obj.sprites.front_default;
+  image.classList.add("pokemonSprite");
   contentSpace.appendChild(image);
+};
+
+const populateMoves = obj => {
+  const contentSpace = document.getElementById("pokeMoves");
+  contentSpace.textContent = "";
+  const title = document.createElement("h2");
+  title.textContent = "Moves: ";
+  contentSpace.appendChild(title);
+
+  const moveTitle = document.createElement("h3");
+  moveTitle.textContent = obj.moves[0].move.name;
+  contentSpace.appendChild(moveTitle);
 };
 
 // Fetch Request
@@ -45,6 +59,10 @@ const searchWithFetch = () => {
     })
     .then(myJSON => {
       populateDOM(myJSON);
+      return myJSON;
+    })
+    .then(myJSON => {
+      populateMoves(myJSON);
     });
 };
 
